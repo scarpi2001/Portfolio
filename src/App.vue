35cafd42b9796e6d8bcb5f07574725138a8ee3ce
sca <posta.email3001@gmail.com>
2023-02-24 17:53:55 +0100

    test 2

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,42 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+
   <div>
-    <h1>Ciao Mondo</h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4FZ0i34TuNoE_pURXcdlQLRXFYVPRDCue8w&usqp=CAU" alt="">
+    <h1>Praticamente il Cojo</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div>
+    <img src="https://www.focus.it/site_stored/imgs/0004/015/sls_nasa4x3.1020x680.jpg" alt="">
+    <h1>il pipo di ste</h1>
+  </div>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss">
+@use './style/general' as*;
+
+div {
+  width: 1000px;
+  margin: 100px auto;
+  text-align: center;
+
+  img {
+    width: 1000px;
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+/* smartphone version */
+@media screen and (max-width: 600px) {
+
+  div {
+    width: 300px;
+    
+    img {
+    width: 300px;
+    }
+  }
+
 }
 </style>
