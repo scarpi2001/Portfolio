e50de76215ec2f76cd91ca35fe594ed666062d7d
sca <posta.email3001@gmail.com>
2023-03-07 00:08:35 +0100

    Footer update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,69 @@
 <script>
 export default {
     name: "Footer",
+    data() {
+        return {
+            contacts: [
+                {
+                    icon: "fa-brands fa-linkedin",
+                    link: "https://www.linkedin.com/in/marco-scarpelli/",
+                },
+                {
+                    icon : "fa-brands fa-github",
+                    link: "https://github.com/scarpi2001",
+                },
+                {   
+                    icon: "fa-brands fa-instagram",
+                    link: "https://www.instagram.com/scarpi_dhc/",
+                },
+            ]
+        }
+    },
 }
 </script>
 
 <template>
-
+    <footer>
+        <div class="contacts_box">
+            <ul>
+                <li v-for="(contact, index) in contacts" :key="index">
+                    <a :href="contact.link">
+                        <font-awesome-icon :icon=contact.icon />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/general" as*;
+@use "../style/partials/variables" as*;
 
+footer {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    height: 60px;
+    background-color: $primary;
+    box-shadow: 8px 0 8px 0 rgba(0, 0, 0, 0.1);
+
+    .contacts_box {
+        font-size: 30px;
+        margin-left: 60px;
+
+        ul {
+            display: flex;
+            gap: 80px;
+
+            li {
+                text-align: center;
+
+                a {
+                    color: $secondary;
+                }
+            }
+        }
+    }
+}
 </style>
