591cc527fcf779af93d1a899d819374172ebb882
sca <posta.email3001@gmail.com>
2023-05-14 17:01:20 +0200

    aggiunte animazioni sul cambio di sezione

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,245 +1,246 @@
 <script>
-import { store } from '../store';
+import { store } from "../store";
 
 export default {
-    name: "Header",
-    data() {
-        return {
-            store,
-            showMobileMenu : false,
-            menu: [
-                {
-                    icon: "fa-solid fa-house-user",
-                    text: "HOME",
-                },
-                {
-                    icon : "fa-solid fa-address-card",
-                    text: "ABOUT",
-                },
-                {   
-                    icon: "fa-solid fa-file",
-                    text: "CV",
-                },
-                {
-                    icon: "fa-solid fa-briefcase",
-                    text: "PROJECTS",
-                }
-            ]
-        }
+  name: "Header",
+  data() {
+    return {
+      store,
+      showMobileMenu: false,
+      menu: [
+        {
+          icon: "fa-solid fa-house-user",
+          text: "HOME",
+        },
+        {
+          icon: "fa-solid fa-address-card",
+          text: "ABOUT",
+        },
+        {
+          icon: "fa-solid fa-file",
+          text: "CV",
+        },
+        {
+          icon: "fa-solid fa-briefcase",
+          text: "PROJECTS",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    toggleMobileMenu() {
+      this.showMobileMenu = !this.showMobileMenu;
     },
 
-    methods: {
-        toggleMobileMenu() {
-            this.showMobileMenu = !this.showMobileMenu; },
-
-        currentVoice(index) {
-            this.store.active = index;
-            this.showMobileMenu = false;
-        },
-        home() {
-            this.store.active = 0;
-        },
-    }
-}
+    currentVoice(index) {
+      this.store.active = index;
+      this.showMobileMenu = false;
+    },
+    home() {
+      this.store.active = 0;
+    },
+  },
+};
 </script>
 
 <template>
+  <header :class="{ dropdown_header: showMobileMenu }">
+    <nav :class="{ dropdown_nav: showMobileMenu }">
+      <figure class="logo" :class="{ hidden: showMobileMenu }">
+        <img src="/logo.svg" alt="logo" @click="home()" />
+      </figure>
 
-    <header :class="{ 'dropdown_header': showMobileMenu }">
+      <div
+        class="hamburger-menu"
+        :class="{ open: showMobileMenu }"
+        @click="toggleMobileMenu()"
+      >
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
 
-        <nav :class="{ 'dropdown_nav': showMobileMenu }">
-
-            <figure class="logo" :class="{ 'hidden': showMobileMenu }">
-                <img src="/logo.svg" alt="logo" @click="home()">
-            </figure>
-
-            <div class="hamburger-menu" :class="{ 'open': showMobileMenu }" @click="toggleMobileMenu()">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
+      <div class="menu" :class="{ active: showMobileMenu }">
+        <ul>
+          <li
+            v-for="(menuVoice, index) in menu"
+            :key="index"
+            @click="currentVoice(index)"
+            :class="{ selected: store.active === index }"
+          >
+            <div>
+              <font-awesome-icon :icon="menuVoice.icon" />
             </div>
-
-            <div class="menu" :class="{ 'active': showMobileMenu }">
-                <ul>
-                    <li v-for="(menuVoice, index) in menu" :key="index" @click="currentVoice(index)"
-                        :class="{ selected: store.active === index }">
-                        <div>
-                            <font-awesome-icon :icon=menuVoice.icon />
-                        </div>
-                        <h4>
-                            {{ menuVoice.text }}
-                        </h4>
-                    </li>
-                </ul>
-            </div>
-
-        </nav>
-
-    </header>
-
+            <h4>
+              {{ menuVoice.text }}
+            </h4>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <style lang="scss" scoped>
-@use "../style/general" as*;
-@use "../style/partials/variables" as*;
+@use "../style/general" as *;
+@use "../style/partials/variables" as *;
 
 header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100px;
+  padding: 0 20px;
+  background-color: $primary;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+  z-index: 999;
+
+  nav {
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100px;
-    padding: 0 20px;
-    background-color: $primary;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+    width: 1200px;
 
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 1200px;
+    .logo {
+      display: flex;
+      align-items: center;
+      width: 70px;
 
-        .logo {
-            display: flex;
-            align-items: center;
-            width: 70px;
-
-            img {
-                border-radius: 50%;
-                cursor: pointer;
-            }
-        }
-
-        .hamburger-menu {
-            display: none;
-        }
-
-        .menu {
-            ul {
-                display: flex;
-                gap: 100px;
-
-                li {
-                    text-align: center;
-                    transition: all 0.2s;
-
-                    div {
-                        font-size: 20px;
-                    }
-
-                    &:hover {
-                        cursor: pointer;
-                        color: $secondary;
-                        scale: 1.2;
-                    }
-                }
-            }
-        }
-        
-
-        .selected {
-            color: $secondary;
-            scale: 1.2;
-        }
-
+      img {
+        border-radius: 50%;
+        cursor: pointer;
+      }
     }
 
+    .hamburger-menu {
+      display: none;
+    }
+
+    .menu {
+      ul {
+        display: flex;
+        gap: 100px;
+
+        li {
+          text-align: center;
+          transition: all 0.2s;
+
+          div {
+            font-size: 20px;
+          }
+
+          &:hover {
+            cursor: pointer;
+            color: $secondary;
+            scale: 1.2;
+          }
+        }
+      }
+    }
+
+    .selected {
+      color: $secondary;
+      scale: 1.2;
+    }
+  }
 }
 
 /* mobile version */
 @media screen and (max-width: 700px) {
+  header {
+    padding: 0 20px;
+    transition: all 0.5s ease;
 
-    header {
-        padding: 0 20px;
-        transition: all 0.5s ease;
+    nav {
+      .logo {
+        color: $secondary;
+        font-size: 18px;
+      }
+      .hamburger-menu {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 43px;
+        right: 20px;
+        gap: 5px;
+        cursor: pointer;
+        transition: all 0.5s ease-out;
 
-        nav {
-            .logo {
-                color: $secondary;
-                font-size: 18px;
-            }
-            .hamburger-menu {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                position: absolute;
-                top: 43px;
-                right: 20px;
-                gap: 5px;
-                cursor: pointer;
-                transition: all 0.5s ease-out;
-
-                .bar {
-                    width: 30px;
-                    height: 3px;
-                    background-color: $secondary;
-                    transition: all 0.5s ease-out;
-                }
-            }
-
-            .hamburger-menu.open {  
-                transform: rotate(180deg);
-                .bar:nth-child(1) {
-                    transform: rotate(45deg) translate(5px, 5px);
-                }
-
-                .bar:nth-child(2) {
-                    opacity: 0;
-                }
-
-                .bar:nth-child(3) {
-                    transform: rotate(-45deg) translate(6px, -6px);
-                }
-            }
-
-            .menu {
-                display: none;
-                padding: 100px 0;
-
-                ul {
-                    flex-direction: column;
-
-                    li {
-                        padding-bottom: 15px;
-                        border-bottom: 1px solid;
-                    }
-                    
-                }
-            }
-
-            @keyframes fadeIn {
-                from { opacity: 0; }
-                to { opacity: 1; }
-            }
-
-            .menu.active ul {
-                animation: fadeIn 1.8s ease forwards;
-            }
-
-            .active {
-                display: block;
-            }
-
-            .hidden {
-                display: none;
-            }
+        .bar {
+          width: 30px;
+          height: 3px;
+          background-color: $secondary;
+          transition: all 0.5s ease-out;
         }
-    }
+      }
 
-    .dropdown_header {
+      .hamburger-menu.open {
+        transform: rotate(180deg);
+        .bar:nth-child(1) {
+          transform: rotate(45deg) translate(5px, 5px);
+        }
+
+        .bar:nth-child(2) {
+          opacity: 0;
+        }
+
+        .bar:nth-child(3) {
+          transform: rotate(-45deg) translate(6px, -6px);
+        }
+      }
+
+      .menu {
+        display: none;
+        padding: 100px 0;
+
+        ul {
+          flex-direction: column;
+
+          li {
+            padding-bottom: 15px;
+            border-bottom: 1px solid;
+          }
+        }
+      }
+
+      @keyframes fadeIn {
+        from {
+          opacity: 0;
+        }
+        to {
+          opacity: 1;
+        }
+      }
+
+      .menu.active ul {
+        animation: fadeIn 1.8s ease forwards;
+      }
+
+      .active {
         display: block;
-        height: 100vh;
-    }
+      }
 
-    .dropdown_nav {
-        display: block;
-        width: auto;
+      .hidden {
+        display: none;
+      }
     }
+  }
 
+  .dropdown_header {
+    display: block;
+    height: 100vh;
+  }
+
+  .dropdown_nav {
+    display: block;
+    width: auto;
+  }
 }
-
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,52 +1,51 @@
 <script>
-import { store } from '../store';
+import { store } from "../store";
 
-import Home from './main_components/Home.vue';
-import About from './main_components/About.vue';
-import Cv from './main_components/Cv.vue';
-import Projects from './main_components/Projects.vue';
+import Home from "./main_components/Home.vue";
+import About from "./main_components/About.vue";
+import Cv from "./main_components/Cv.vue";
+import Projects from "./main_components/Projects.vue";
 
 export default {
-    name: "Main",
+  name: "Main",
 
-    components: {
-        Home,
-        About,
-        Cv,
-        Projects,
-    },
+  components: {
+    Home,
+    About,
+    Cv,
+    Projects,
+  },
 
-    data() {
-        return {
-            store,
-            components: [Home, About, Cv, Projects]
-        }
-    },
-}
+  data() {
+    return {
+      store,
+      components: [Home, About, Cv, Projects],
+    };
+  },
+};
 </script>
 
 <template>
-    <!-- in questo modo con il componente dinamico i componenti vengono renderizzati e distrutti ogni volta che active cambia (questo ottimizza il rendering iniziale) -->
-    <main>
-        <component :is="components[store.active]" />
-    </main>
+  <!-- in questo modo con il componente dinamico i componenti vengono renderizzati e distrutti ogni volta che active cambia (questo ottimizza il rendering iniziale) -->
+  <main>
+    <component :is="components[store.active]" />
+  </main>
 
-    <!-- in questo modo i componenti vengano renderizzati dal principio e mostrati o nascosti in base alla variabile visible (ottimizza il toggle delle voci di menu) -->
-    <!-- <main>
+  <!-- in questo modo i componenti vengano renderizzati dal principio e mostrati o nascosti in base alla variabile visible (ottimizza il toggle delle voci di menu) -->
+  <!-- <main>
         <component :is="components[store.active]" v-show="store.visible" />
     </main> -->
 </template>
 
 <style lang="scss" scoped>
-@use "../style/general" as*;
-@use "../style/partials/variables" as*;
+@use "../style/general" as *;
+@use "../style/partials/variables" as *;
 
 main {
-    margin-top: 100px;
-    padding: 0 20px;
-    background-color: $background;
-    overflow: hidden;
-    overflow-y: auto;
+  margin-top: 100px;
+  padding: 0 20px;
+  background-color: $background;
+  overflow: hidden;
+  overflow-y: auto;
 }
-
 </style>
