733dd27c0697dc654f750101d7b1aa1ab4e92162
sca <posta.email3001@gmail.com>
2023-05-14 19:19:12 +0200

    Vue Router setup

--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import { faLinkedin as fasLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faGithub as fasGithub } from '@fortawesome/free-brands-svg-icons'
-import { faInstagram as fasInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faHomeUser as fasHomeUser } from '@fortawesome/free-solid-svg-icons'
-import { faAddressCard as fasAddressCard } from '@fortawesome/free-solid-svg-icons'
-import { faFile as fasFile } from '@fortawesome/free-solid-svg-icons'
-import { faBriefcase as fasBrieffaBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin as fasLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub as fasGithub } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram as fasInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faHomeUser as fasHomeUser } from "@fortawesome/free-solid-svg-icons";
+import { faAddressCard as fasAddressCard } from "@fortawesome/free-solid-svg-icons";
+import { faFile as fasFile } from "@fortawesome/free-solid-svg-icons";
+import { faBriefcase as fasBrieffaBriefcase } from "@fortawesome/free-solid-svg-icons";
 
-library.add(fasInstagram, fasGithub, fasLinkedin, fasHomeUser, fasAddressCard, fasFile, fasBrieffaBriefcase)
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+library.add(
+  fasInstagram,
+  fasGithub,
+  fasLinkedin,
+  fasHomeUser,
+  fasAddressCard,
+  fasFile,
+  fasBrieffaBriefcase
+);
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "../components/main_components/Home.vue";
+import About from "../components/main_components/About.vue";
+import Cv from "../components/main_components/Cv.vue";
+import Projects from "../components/main_components/Projects.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/cv", component: Cv },
+  { path: "/projects", component: Projects },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
